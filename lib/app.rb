bbfbe435501886d8e68148a7a1c8bbf16f6cb05d
Sebastián Morales <sebasuni96@gmail.com>
2023-01-28 01:17:49 -0300

    Fix: request

--- lib/app.rb
+++ lib/app.rb
@@ -5,6 +5,6 @@ set :bind, '0.0.0.0'
 set :port, 4567
 
 get '/run_script' do
-  output = `ruby dos_titulos/main.rb`
+  output = `ruby /app/lib/dos_titulos/main.rb`
   { output: output }.to_json
 end
